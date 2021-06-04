c6f0b2c014070086b009e0665c13599c56315abc
tejas-trivedi <500077185@stu.upes.ac.in>
2021-06-04 23:24:56 +0530

    gcloud deployed

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.8.5
+FROM python:alpine3.8
 
 RUN mkdir place
 
@@ -6,7 +6,12 @@ WORKDIR /place
 
 COPY . .
 
+#ENV PORT 8080
+
+
 RUN python3 -m pip install --upgrade pip
 RUN pip3 install -r requirements.txt 
+
+#CMD exec gunicorn --bind :$PORT --workers 1 --threads 8 app:app
 
 CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -232,7 +232,9 @@ def success():
         os.remove(f.filename)
         return render_template("success.html", label1=label1,label2=label2,label3=label3)
 
+
 if __name__ == '__main__':
     #port = int(os.environ.get('PORT', 5000))
     #app.run(host='0.0.0.0', port=port, debug=True)
+    app.debug = True
     app.run(threaded=True, port=5000)
